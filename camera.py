4d03841e6e4867a1fbe407ed83cdae1490e4f30c
Sucram314 <99621225+Sucram314@users.noreply.github.com>
2025-01-31 22:38:32 -0500

    cleaned up some variables

--- camera.py
+++ camera.py
@@ -1,20 +1,18 @@
-import pygame
-
-FOLLOW_RATE = 0.2
-
 class Camera:
-    def __init__(self,x,y,WIDTH,HEIGHT):
+    def __init__(self,x,y,screen_width,screen_height,follow_rate=0.2):
         self.x = x
         self.y = y
 
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        self.follow_rate = follow_rate
 
     def follow(self, x, y):
-        self.x += (x - self.x) * FOLLOW_RATE
-        self.y += (y - self.y) * FOLLOW_RATE
+        self.x += (x - self.x) * self.follow_rate
+        self.y += (y - self.y) * self.follow_rate
 
     def to_screen(self,x,y):
-        screen_x = x - self.x + self.WIDTH/2
-        screen_y = self.y - y + self.HEIGHT/2
+        screen_x = x - self.x + self.screen_width/2
+        screen_y = self.y - y + self.screen_height/2
         return screen_x, screen_y 
